d535654f70a04e8ce0bc71eca848724fd7d2ad1b
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2022-07-14 21:31:34 +0200

    feat(read-wording): implement domain with working test

---
+++ src/main/java/student/valle/Amount.java
@@ -0,0 +1,13 @@
+package student.valle;
+
+public class Amount {
+    private final Double value;
+
+    public Amount(Double value) {
+        this.value = value;
+    }
+
+    public Double getRoundedValue() {
+        return value;
+    }
+}
