43efe957f2dd612d2080ffc4aead6280ec1c24e1
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2022-07-14 23:20:43 +0200

    feat(ProductTests): implement working domain for getTaxedImportedBasicNecessitiesProduct test

--- src/main/java/student/valle/Product.java
+++ src/main/java/student/valle/Product.java
@@ -15,7 +15,10 @@ public class Product {
     public Double calculateTaxedPrice() {
         if (types.contains(ProductType.BOOK)) return new Amount( price + (price * 10.00/100)).getRoundedValue();
 
-        if (types.contains(ProductType.BASIC_NECESSITIES)) return new Amount(price).getRoundedValue();
+        if (types.contains(ProductType.BASIC_NECESSITIES)) {
+            if (types.contains(ProductType.IMPORTED)) return new Amount(price + (price * 5.00/100)).getRoundedValue();
+            return new Amount(price).getRoundedValue();
+        }
 
         return new Amount(price + (price * 20.00/100)).getRoundedValue();
     }
