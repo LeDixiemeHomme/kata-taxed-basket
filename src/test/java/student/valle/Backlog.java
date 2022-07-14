24ffdf3083586c91beb8f0d9f529cd1037f6d764
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2022-07-14 21:16:09 +0200

    feat(read-wording): tests found after reading the wording inserted in Backlog

---
+++ src/test/java/student/valle/Backlog.java
@@ -0,0 +1,84 @@
+package student.valle;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@Disabled
+class Backlog {
+
+    @Test
+    void getTaxedBasicNecessitiesProduct() {
+    }
+
+    @Test
+    void getTaxedBookProduct() {
+    }
+
+    @Test
+    void getTaxedRegularProduct() {
+    }
+
+    @Test
+    void getTaxedImportedBasicNecessitiesProduct() {
+    }
+
+    @Test
+    void getTaxedImportedBookProduct() {
+    }
+
+    @Test
+    void getTaxedImportedRegularProduct() {
+    }
+
+    @Test
+    void getRoundedAmountWith0() {
+    }
+
+    @Test
+    void getRoundedAmountWith5() {
+    }
+
+    @Test
+    void getRoundedAmountWithBetween1And4() {
+    }
+
+    @Test
+    void getRoundedAmountWithBetween6And9() {
+    }
+
+    @Test
+    void getAmountOfBasketWithNoProduct() {
+    }
+
+    @Test
+    void getAmountOfBasketWithOneProduct() {
+    }
+
+    @Test
+    void getAmountOfBasketWithTwoProducts() {
+    }
+
+    @Test
+    void getAmountOfBasketTaxesWithNoProduct() {
+    }
+
+    @Test
+    void getAmountOfBasketTaxesWithOneProduct() {
+    }
+
+    @Test
+    void getAmountOfBasketTaxesWithTwoProducts() {
+    }
+
+    @Test
+    void printReceiptWithNoProduct() {
+    }
+
+    @Test
+    void printReceiptWithOneProduct() {
+    }
+
+    @Test
+    void printReceiptWithTwoProducts() {
+    }
+}
