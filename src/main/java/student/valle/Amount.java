f0dbfa0793381874d4a2cb5e91a744c4947db989
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2022-07-14 22:30:22 +0200

    feat(AmountTests): implement domain for number between .06 and .09

--- src/main/java/student/valle/Amount.java
+++ src/main/java/student/valle/Amount.java
@@ -18,10 +18,11 @@ public class Amount {
         List<String> splitValue = Arrays.asList(String.valueOf(twoDecimalDouble * 10).split("\\."));
 
         int integerPart = Integer.parseInt(splitValue.get(0));
-        int decimalPart = Integer.parseInt(splitValue.get(1));
+        int decimalPart = Integer.parseInt(splitValue.get(1).substring(0,1));
 
         if (decimalPart >= 1 && decimalPart <= 4) return (integerPart + 0.5) / 10.00;
+        if (decimalPart >= 6 && decimalPart <= 9) return integerPart / 10.00 + 1;
 
-        return this.value - 0.1;
+        return this.value;
     }
 }
