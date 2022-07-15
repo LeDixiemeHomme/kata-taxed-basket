032f908ace186b634b9a1f6883df98264aaabc66
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2022-07-15 09:59:05 +0200

    feat(ProductTests): implement none working printReceiptWithOneProduct test

--- src/test/java/student/valle/ReceiptPrinterTests.java
+++ src/test/java/student/valle/ReceiptPrinterTests.java
@@ -19,4 +19,17 @@ public class ReceiptPrinterTests {
                 "\n" +
                 "Total : 0.00", receiptPrinter.printBasketReceipt());
     }
+
+    @Test
+    void printReceiptWithOneProduct() {
+        Product product = Product.builder().name("book").types(Arrays.asList(ProductType.BOOK)).price(25.00).build();
+        ProductBasket productBasket = new ProductBasket(Arrays.asList(product));
+
+        ReceiptPrinter receiptPrinter = new ReceiptPrinter(productBasket);
+
+        assertEquals("1 book à 25.00 : 27.50\n" +
+                "Montant des taxes : 2.50\n" +
+                "\n" +
+                "Total : 27.50", receiptPrinter.printBasketReceipt());
+    }
 }
